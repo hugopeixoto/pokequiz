d9238886d080532609621980db4ae6d0b5450d74
Hugo Peixoto <hugo.peixoto@gmail.com>
2020-09-05 01:14:03 +0100

    Add basic hardcoded query

---
+++ potato.py
@@ -0,0 +1,36 @@
+import pokedex
+
+import pokedex.cli.search
+import pokedex.db
+import pokedex.db.load
+import pokedex.db.util
+import pokedex.lookup
+
+from pokedex import defaults
+
+def get_session(args):
+    engine_uri, got_from = defaults.get_default_db_uri_with_origin()
+    return pokedex.db.connect(engine_uri)
+
+def get_lookup(args, session=None, recreate=False):
+    index_dir, got_from = defaults.get_default_index_dir_with_origin()
+    lookup = pokedex.lookup.PokedexLookup(index_dir, session=session)
+
+    return lookup
+
+
+def poop(query):
+    session = get_session([])
+
+    pokemon = session.query(pokedex.db.tables.Pokemon).all()
+
+    if query == "pokedex.national <= 151":
+        pokemon = [
+            p for p in pokemon
+            if any(d for d in p.species.dex_numbers if d.pokedex_id == 1 and d.pokedex_number <= 151)
+        ]
+
+    return pokemon
+
+for p in poop("pokedex.national <= 151"):
+    print(p.identifier)
