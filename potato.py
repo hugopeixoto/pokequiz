1a73b6f11622a8af6ffbd2441ea4643c71a3989a
Hugo Peixoto <hugo.peixoto@gmail.com>
2020-09-08 01:56:04 +0100

    Clean up imports and unused pokedex code

--- potato.py
+++ potato.py
@@ -1,24 +1,13 @@
-import pokedex
 import sys
 import re
 
-import pokedex.cli.search
+import pokedex
 import pokedex.db
-import pokedex.db.load
-import pokedex.db.util
-import pokedex.lookup
-
-from pokedex import defaults
+import pokedex.defaults
 
 def get_session(args):
-    engine_uri, got_from = defaults.get_default_db_uri_with_origin()
+    engine_uri, got_from = pokedex.defaults.get_default_db_uri_with_origin()
     return pokedex.db.connect(engine_uri)
-
-def get_lookup(args, session=None, recreate=False):
-    index_dir, got_from = defaults.get_default_index_dir_with_origin()
-    lookup = pokedex.lookup.PokedexLookup(index_dir, session=session)
-
-    return lookup
 
 def pokedex_regional(pokemon_instance):
     return [
